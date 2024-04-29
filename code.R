b60577b7e0cfa3572889f608551a80dc321436ba
Dia Leor Paule <dia.paule@gmail.com>
2024-04-30 00:03:16 +0800

    Update code.

--- code.R
+++ code.R
@@ -1,1 +1,3 @@
-2+2
+femaleMiceWeights <- read.csv("mice-data/femaleMiceWeights.csv")
+femaleControlsPopulation <- read.csv("mice-data/femaleControlsPopulation.csv")
+mice_pheno <- read.csv("mice-data/mice_pheno.csv")
