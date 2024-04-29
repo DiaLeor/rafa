20218ee31e4c5e3b45960cf727bee11e88f178f0
Dia Leor Paule <dia.paule@gmail.com>
2024-04-30 01:23:26 +0800

    Update code.

--- code.R
+++ code.R
@@ -1,3 +1,32 @@
-femaleMiceWeights <- read.csv("mice-data/femaleMiceWeights.csv")
-femaleControlsPopulation <- read.csv("mice-data/femaleControlsPopulation.csv")
-mice_pheno <- read.csv("mice-data/mice_pheno.csv")
+# Much of the code below, I've already learned in other Harvard Online courses.
+# However, to earn a program certificate in Data Analysis for Life Sciences,
+# you must earn the Statistics and R course certificate.
+
+# Store raw data into objects
+dat <- read.csv("mice-data/femaleMiceWeights.csv")
+
+# load dplyr
+library(dplyr)
+
+# filter() lets you filter certain rows of the data
+controls <- filter(dat, Diet == "chow")
+controls
+
+# select() lets you select certain columns of the data 
+controls <- select(controls, Bodyweight)
+
+# to convert this into a numerical vector, we can use unlist()
+unlist(controls)
+
+# dplyr is also the package that allows us to use the pipe operator, which
+# lets us do a series of commands in one line
+controls <- dat %>% filter(Diet == "chow") %>%
+  select(Bodyweight) %>%unlist()
+mean(controls)
+
+library(downloader)
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+
+
